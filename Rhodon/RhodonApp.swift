b7ee46bfd6893298f6e96f1fca4bb94afcdb3710
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2026-05-15 10:49:24 +0200

    Initial Commit

---
+++ Rhodon/RhodonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhodonApp.swift
+//  Rhodon
+//
+//  Created by Matthias Wallner-Géhri on 15.05.26.
+//
+
+import SwiftUI
+
+@main
+struct RhodonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RhodonTests/RhodonTests.swift
@@ -0,0 +1,19 @@
+//
+//  RhodonTests.swift
+//  RhodonTests
+//
+//  Created by Matthias Wallner-Géhri on 15.05.26.
+//
+
+import Testing
+@testable import Rhodon
+
+struct RhodonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
